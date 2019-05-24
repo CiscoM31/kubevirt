ff33cb7b0161037288e0b9fe1036dfd9d0a81cf7
Prashanth Buddhala <pbuds@cisco.com>
2019-05-24 16:36:05 -0700

    Revert "Enhance the Cisco provider 'build' function to log onto worker nodes and perform a 'docker pull'"
    This reverts commit 3cef94372a6fd90e118356050152d01512b39579.

--- cluster/cisco/provider.sh
+++ cluster/cisco/provider.sh
@@ -6,24 +6,7 @@ function up() {
 }
 
 function build() {
-    local SSH_USER=""
-    [ -n "$KB_NODE_USER" ] && SSH_USER="-l $KB_NODE_USER"
-    # Make sure we have any required variables before starting
-    if [ -z ${DOCKER_PREFIX+x} ]; then echo "DOCKER_PREFIX variable not set"; exit 1; fi
-
-    # Build and push things
-    make manifests docker publish
-
-    # Then make sure we are using latest on all the nodes, this will require SSH keys to be setup
-    for node in `_kubectl get nodes | tail -n +2 | awk '{print $1}'`; do
-        echo "Connecting to host $node"
-        ssh-add -l
-        ssh $SSH_USER $node docker pull $DOCKER_PREFIX/virt-api
-        ssh $SSH_USER $node docker pull $DOCKER_PREFIX/virt-handler
-        ssh $SSH_USER $node docker pull $DOCKER_PREFIX/virt-launcher
-        ssh $SSH)$node docker pull $DOCKER_PREFIX/virt-controller
-    done
-
+    make manifests docker
 }
 
 function _kubectl() {
