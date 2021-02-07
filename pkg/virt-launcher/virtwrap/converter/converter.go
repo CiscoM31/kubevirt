1ec8b362994fe2364921033b0751b42e14a52838
Prashanth Buddhala <pbudds@gmail.com>
2021-02-07 08:13:23 +0000

    resolveconflicts: add alias name to usb controller to fix migration failure

--- pkg/virt-launcher/virtwrap/converter/converter.go
+++ pkg/virt-launcher/virtwrap/converter/converter.go
@@ -1461,6 +1461,9 @@ func Convert_v1_VirtualMachineInstance_To_api_Domain(vmi *v1.VirtualMachineInsta
 			Type:  "usb",
 			Index: "0",
 			Model: "qemu-xhci",
+			Alias: &api.Alias{
+				Name: "usbController",
+			},
 		})
 	}
 
@@ -1469,6 +1472,9 @@ func Convert_v1_VirtualMachineInstance_To_api_Domain(vmi *v1.VirtualMachineInsta
 			Type:  "scsi",
 			Index: "0",
 			Model: translateModel(c, "virtio"),
+			Alias: &api.Alias{
+				Name: "scsiController",
+			},
 		})
 	}
 
